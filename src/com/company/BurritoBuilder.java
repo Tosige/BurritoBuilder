54cec0862291ca430b1755780270929d4759a926
Tosin Ige <tosin.ige@gmail.com>
2019-04-26 14:05:05 -0400

    adding price calculator

--- src/com/company/BurritoBuilder.java
+++ src/com/company/BurritoBuilder.java
@@ -1,68 +1,63 @@
 package com.company;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.*;
+import java.util.List;
 
 public class BurritoBuilder {
 
-//    static String picker(ArrayList arrayx) {
-//        //ArrayList<String> arrayx = new ArrayList<>();
-//        Random rand = new Random();
-//        int picknumber = rand.nextInt(arrayx.size());
-//        System.out.print(arrayx.get(picknumber));
-//        return arrayx.get(picknumber);
-//    }
+    //Method randomly picks an item from an arraylist
+    static String picker(List<String> arrayx) {
+        Random rand = new Random();
+        int picknumber = rand.nextInt(arrayx.size());
+        //System.out.print(arrayx.get(picknumber));
+        String picked = arrayx.get(picknumber);
+        Boolean No = picked.contains("no");
+        if (!No)
+        return arrayx.get(picknumber);
+    }
+    //Method randomly select a boolean result for extra ingredients list
+    static String picker2(List<Boolean> arrayz) {
+        Random rand = new Random();
+        Boolean bpicker = rand.nextBoolean();
+        //System.out.print(bpicker);
+        if (bpicker == true) {
+            return "Yes";
+        } else {
+            return "No";
+        }
+        //Boolean picked2 = arrayz.get(bpicker);
+        //return arrayz.get(picknumber);
+        //return bpicker;
+    }
+
+    static int pricer(){
+        if (picker( ) )
+    }
 
     public static void main(String[] args) {
 	//This program allows a robot to assemble 25 burritos. A random
     //generator customizes the combination of ingredients in each burrito
 
-        ArrayList<String> burritos = new ArrayList<>();
-        ArrayList<String> rice = new ArrayList<>();
-        ArrayList<String> meat = new ArrayList<>();
-        ArrayList<String> beans = new ArrayList<>();
-        ArrayList<String> salsa = new ArrayList<>();
-        ArrayList<String> veggies = new ArrayList<>();
+        //Creating Lists of category options
+       List<String> rice = Arrays.asList("white rice", "brown rice", "no rice");
+       List<String> meat = Arrays.asList( "chicken", "steak", "carnidas", "chorizo", "sofritas", "veggies");
+       List<String> beans = Arrays.asList( "pinto beans", "black beans", "no beans", "all beans");
+       List<String> salsa = Arrays.asList( "hot salsa", "medium salsa", "mild salsa", "no salsa", "all salsa");
+       List<String> veggies = Arrays.asList( "lettuce", "fajita veggies", "no veggies", "all veggies");
+       List<Boolean> Cheese = Arrays.asList(true, false);
+       List<Boolean> Guac = Arrays.asList(true, false);
+       List<Boolean> Queso = Arrays.asList(true, false);
+       List<Boolean> sCream = Arrays.asList(true, false);
 
-       //List<String> rice = Arrays.asList( "white rice", "brown rice", "no rice");
-        rice.add("white rice");
-        rice.add("brown rice");
-        rice.add("no rice");
-        meat.add("chicken");
-        meat.add("steak");
-        meat.add("carnidas");
-        meat.add("chorizo");
-        meat.add("sofritas");
-        meat.add("veggies");
-        beans.add("pinto beans");
-        beans.add("black beans");
-        beans.add("no beans");
-        salsa.add("mild salsa");
-        salsa.add("medium salsa");
-        salsa.add("hot salsa");
-        salsa.add("no salsa");
-        salsa.add("all salsa");
-        veggies.add("lettuce");
-        veggies.add("fajita veggies");
-        veggies.add("no veggie");
-        veggies.add("all veggies");
+        //For
+                for (int i=0; i<25; i++) {
+            System.out.print("Burrito " + (i+1) + ": " + picker(rice) + ", ");
+            System.out.println(picker(meat) + ", " + picker(beans) + " ," + picker(salsa) + ", " + picker(veggies));
+            System.out.println("Extras: " + "Cheese-" + picker2(Cheese) + " | Guac-" + picker2(Guac) + " | Queso-" + picker2(Queso) + " | Sour Cream-" + picker2(sCream));
+            System.out.println("Price: ");
+            System.out.println();
 
-        for (int i=0; i<25; i++) {
-            Random rand = new Random();
-            int ricepick = rand.nextInt(rice.size());
-            String ricex = rice.get(ricepick);
-            int meatpick = rand.nextInt(meat.size());
-            String meatx = meat.get(meatpick);
-            int salsapick = rand.nextInt(salsa.size());
-            String salsax = salsa.get(salsapick);
-            int vegpick = rand.nextInt(veggies.size());
-            String vegx = veggies.get(vegpick);
-            int beanspick = rand.nextInt(beans.size());
-            String beansx = beans.get(beanspick);
-            System.out.print("Burrito " + (i+1) + ": " + ricex + ", ");
-            System.out.println(meatx + ", " + beansx + " ," + salsax + ", " + vegx );
         }
 
     }
